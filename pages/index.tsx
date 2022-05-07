f5669f5b47a0b41636aa84e4074a2f6164be00c6
Ivan Porollo <iporollo@gmail.com>
2022-05-07 13:50:34 -0700

    add gtag

--- pages/index.tsx
+++ pages/index.tsx
@@ -79,6 +79,23 @@ const Home: NextPage = () => {
         <title>Triggerify NFT</title>
         <meta name="description" content="Triggerify NFT" />
         <link rel="icon" href="/favicon.ico" />
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
       </Head>
       <AppHeader />
       <WagmiProvider client={wagmiClient}>
