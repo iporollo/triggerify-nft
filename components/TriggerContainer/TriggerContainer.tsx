66302c494fcad56fbe57be1c5c0054429485c29f
Ivan Porollo <iporollo@gmail.com>
2022-05-12 12:40:29 -0700

    implement minting through NFTPort

--- components/TriggerContainer/TriggerContainer.tsx
+++ components/TriggerContainer/TriggerContainer.tsx
@@ -62,11 +62,19 @@ const TriggerContainer = ({
 }: TriggerContainerProps) => {
   const [triggerLevel, setTriggerLevel] = useState<number>(5);
   const [renderingGif, setRenderingGif] = useState<boolean>(false);
+  const [isMinting, setIsMinting] = useState<boolean>(false);
   const [isSaving, setIsSaving] = useState<boolean>(false);
   const [gifProgress, setGifProgress] = useState<number>(0);
 
+  const NFT_NAME = selectedNft
+    ? `Triggered ${selectedNft.metadata.name} (Level ${triggerLevel})`
+    : '';
+
+  const FILE_NAME = `${NFT_NAME}.gif`;
+
   const handleMintClick = () => {
-    console.log('minting logic');
+    setRenderingGif(true);
+    setIsMinting(true);
   };
 
   const handleSaveClick = () => {
@@ -82,12 +90,47 @@ const TriggerContainer = ({
   const gifFinishedCallback = (blob: Blob) => {
     setRenderingGif(false);
     if (isSaving) {
-      const fileName = `Triggered ${selectedNft.metadata.name} (Level ${triggerLevel}).gif`;
       const tempLink = document.createElement('a');
       tempLink.href = URL.createObjectURL(blob);
-      tempLink.setAttribute('download', fileName);
+      tempLink.setAttribute('download', FILE_NAME);
       tempLink.click();
+    } else if (isMinting) {
+      mintNft(blob);
     }
+    setIsSaving(false);
+    setIsMinting(false);
+  };
+
+  const mintNft = async (blob: Blob) => {
+    const form = new FormData();
+
+    var file = new File([blob], FILE_NAME, {
+      type: 'image/gif',
+      lastModified: Date.now(),
+    });
+    form.append('file', file, FILE_NAME);
+
+    const options = {
+      method: 'POST',
+      body: form,
+      headers: {
+        Authorization: process.env.NEXT_PUBLIC_NFTPORT_API_KEY || '',
+      },
+    };
+
+    const response = await fetch(
+      'https://api.nftport.xyz/v0/mints/easy/files?' +
+        new URLSearchParams({
+          chain: 'polygon',
+          name: NFT_NAME,
+          description: `Created by triggerify.xyz - A triggered version of your NFT - ${NFT_NAME}`,
+          mint_to_address: accountData?.address,
+        }),
+      options
+    );
+
+    const result = await response.json();
+    console.log(result);
   };
 
   const getNftImage = () => {
